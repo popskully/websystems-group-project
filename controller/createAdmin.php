cddf7ece435e5781f409efba4cf2a4ee52af9e77
popskully <davionlounds85@gmail.com>
2021-11-20 11:41:04 -0500

    hotfix: email already exist bug

--- controller/createAdmin.php
+++ controller/createAdmin.php
@@ -8,25 +8,30 @@ if (isset($_POST['submit'])) {
     $role = $_POST['role'];
     $passEn = md5($pass);
 
-    $checkEmail = "SELECT * FROM librarian where email='$email'";
-    $exist = mysqli_query($conn, $checkEmail);
+    // $checkEmail = "SELECT * FROM librarian where email='$email'";
+    // $exist = mysqli_query($conn, $checkEmail);
 
     if (empty($email) || empty($pass)|| empty($name)) {
         echo ("<script>alert('Fields cannot be empty');</script>");
         header('location:../account.php');
         exit();
-    } else if ($exist){
-        // while ($row = mysqli_fetch_assoc($exist)) {
-            echo ("<script>alert('Email already taken');</script>");
-            header('location:../error.php');
-        // }
     } else {
+
+        $query = "SELECT * from librarian where email = '$email'";
+        $valid = $conn->query($query);
+
+        if($valid->num_rows > 0) {
+            $row = $valid->fetch_assoc();
+                 echo ("<script>console.log('".$row['email']."');</script>");
+                 header('location:../error.php');
+                 exit();
+        }
 
         $sql = "INSERT INTO librarian (name, type, email, password) VALUES ('$name', '$role', '$email', '$passEn')";
         //mysqli_query use the connection string and the query to run the query
         if (mysqli_query($conn, $sql)) {
             echo ("<script>console.log('Record inserted');</script>");
-            header('location:../account.php');
+            header('location:../displayUser.php');
         } else {
             echo ("<script>console.log('Records not inserted');</script>");
             header('location:../account.php');
